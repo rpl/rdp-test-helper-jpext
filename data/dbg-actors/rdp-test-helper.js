41508e2e8b18264a9cf11fc8fe97e04867919f25
Luca Greco <luca.greco@alcacoop.it>
2013-08-04 22:38:41 +0200

    add MINIMIZE and RDP_LISTEN_PORT env configs + exitApp command in the debugger server actor

--- data/dbg-actors/rdp-test-helper.js
+++ data/dbg-actors/rdp-test-helper.js
@@ -28,6 +28,18 @@ function RDPTestHelperActor(aConnection) {
   promise = Cu.import("resource://gre/modules/commonjs/sdk/core/promise.js").Promise;
 }
 
+function quit (aForceQuit)
+{
+  var appStartup = Components.classes['@mozilla.org/toolkit/app-startup;1'].
+    getService(Components.interfaces.nsIAppStartup);
+
+  // eAttemptQuit will try to close each XUL window, but the XUL window can cancel the quit
+  // process if there is unsaved data. eForceQuit will quit no matter what.
+  var quitSeverity = aForceQuit ? Components.interfaces.nsIAppStartup.eForceQuit :
+                                  Components.interfaces.nsIAppStartup.eAttemptQuit;
+  appStartup.quit(quitSeverity);
+}
+
 RDPTestHelperActor.prototype = {
   actorPrefix: "rdpTestHelper",
 
@@ -40,6 +52,12 @@ RDPTestHelperActor.prototype = {
         error: error,
         message: message
       });
+  },
+
+  exitApp: function() {
+    debug("exitApp");
+
+    quit(true);
   },
 
   openTab: function (aRequest) {
@@ -75,7 +93,8 @@ RDPTestHelperActor.prototype = {
  * The request types this actor can handle.
  */
 RDPTestHelperActor.prototype.requestTypes = {
-  "openTab": RDPTestHelperActor.prototype.openTab
+  "openTab": RDPTestHelperActor.prototype.openTab,
+  "exitApp": RDPTestHelperActor.prototype.exitApp
 };
 
 DebuggerServer.addGlobalActor(RDPTestHelperActor, "RDPTestHelperActor");
